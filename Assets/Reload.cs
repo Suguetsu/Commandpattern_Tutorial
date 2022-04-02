22ac18c69ec04dde1e6d16a03a69a95af766be42
suguetsu <amarelo_77@hotmail.com>
2022-04-02 17:04:54 -0300

    STUDY

---
+++ Assets/Reload.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reload : MonoBehaviour
+{
+    public void Load()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+}
